fa81eff4fb80c3ef39f4b2d1426c36a6b46e646b
sivanadupuru <39900307+sivanadupuru@users.noreply.github.com>
2018-06-18 16:22:33 +0530

    Add files via upload
    facebook intgration

---
+++ src/main/java/com/siva/lowes/SpringFaceBookIntgrationApplication.java
@@ -0,0 +1,12 @@
+package com.siva.lowes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFaceBookIntgrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFaceBookIntgrationApplication.class, args);
+	}
+}
